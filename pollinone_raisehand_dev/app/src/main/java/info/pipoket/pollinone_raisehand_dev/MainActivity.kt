1cc46b111c4135433c31d9f8d736e8f2b720389f
Woosuk Suh <pipoket88@gmail.com>
2017-05-03 19:49:55 +0900

    Implement basic hand-up/down detection

--- pollinone_raisehand_dev/app/src/main/java/info/pipoket/pollinone_raisehand_dev/MainActivity.kt
+++ pollinone_raisehand_dev/app/src/main/java/info/pipoket/pollinone_raisehand_dev/MainActivity.kt
@@ -2,21 +2,38 @@ package info.pipoket.pollinone_raisehand_dev
 
 import android.content.Context
 import android.hardware.SensorManager
+import android.media.MediaActionSound
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mVoteMotionDetector: VoteMotionDetector
+    private lateinit var mSound: MediaActionSound
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val sensorManager: SensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mVoteMotionDetector = VoteMotionDetector(sensorManager)
+        mVoteMotionDetector = VoteMotionDetector(
+                sensorManager, this::onHandUpDetected, this::onHandDownDetected)
+
+        // Following callback setters are only for debugging
         mVoteMotionDetector.setAccelerometerUpdateCallback(this::onAccelerometerUpdate)
         mVoteMotionDetector.setGravityUpdateCallback(this::onGravityUpdate)
+
+        // Following initialization is only for debugging
+        mSound = MediaActionSound()
+        mSound.load(MediaActionSound.START_VIDEO_RECORDING)
+        mSound.load(MediaActionSound.SHUTTER_CLICK)
+    }
+
+    override fun onDestroy() {
+        mSound.release()
+        mVoteMotionDetector.stopDetection()
+        super.onDestroy()
     }
 
     override fun onResume() {
@@ -29,7 +46,16 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
     }
 
+    fun onHandUpDetected() {
+        mSound.play(MediaActionSound.START_VIDEO_RECORDING)
+    }
+
+    fun onHandDownDetected() {
+        mSound.play(MediaActionSound.SHUTTER_CLICK)
+    }
+
     fun onAccelerometerUpdate(valueX: Float, valueY: Float, valueZ: Float) {
+        // This callback is only for debugging
         val labelX: TextView = findViewById(R.id.textAccXValue) as TextView
         val labelY: TextView = findViewById(R.id.textAccYValue) as TextView
         val labelZ: TextView = findViewById(R.id.textAccZValue) as TextView
@@ -40,6 +66,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onGravityUpdate(valueX: Float, valueY: Float, valueZ: Float) {
+        // This callback is only for debugging
         val labelX: TextView = findViewById(R.id.textGravityXValue) as TextView
         val labelY: TextView = findViewById(R.id.textGravityYValue) as TextView
         val labelZ: TextView = findViewById(R.id.textGravityZValue) as TextView
--- pollinone_raisehand_dev/app/src/main/java/info/pipoket/pollinone_raisehand_dev/VoteMotionDetector.kt
+++ pollinone_raisehand_dev/app/src/main/java/info/pipoket/pollinone_raisehand_dev/VoteMotionDetector.kt
@@ -1,8 +1,10 @@
 package info.pipoket.pollinone_raisehand_dev
 
 import android.hardware.*
+import android.os.Handler
 import android.util.Log
-import java.lang.reflect.Type
+import android.view.MotionEvent
+
 
 class LinearAccelerometerListener : SensorEventListener {
     private val TAG: String = javaClass.simpleName
@@ -38,9 +40,24 @@ class GravityListener : SensorEventListener {
     }
 }
 
+data class Vector3(val x: Float, val y: Float, val z: Float) {
+    val length: Float = Math.sqrt(x.toDouble() * x + y * y + z * z).toFloat()
+
+    operator fun plus(o: Vector3): Vector3 = Vector3(x + o.x, y + o.y, z + o.z)
+    operator fun minus(o: Vector3): Vector3 = Vector3(x - o.x, y - o.y, z - o.z)
+    operator fun times(o: Vector3): Float = x * o.x + y * o.y + z * o.z
+    operator fun unaryMinus(): Vector3 = Vector3(-x, -y, -z)
+
+    fun getUnitVector(): Vector3 {
+        return Vector3(x / length, y / length, z / length)
+    }
+}
+
 typealias TypeSensorCallback = (Float, Float, Float) -> Unit
 
 class VoteMotionDetector {
+    private val TAG: String = javaClass.simpleName
+
     private val mSensorManager: SensorManager
 
     private val mSensorLinearAccelerometer: Sensor
@@ -51,25 +68,53 @@ class VoteMotionDetector {
 
     private var mGravityUpdateCallback: TypeSensorCallback? = null
     private var mAccelerometerUpdateCallback: TypeSensorCallback? = null
+    private val mHandUpCallback: ()->Unit
+    private val mHandDownCallback: ()->Unit
 
-    constructor(sensorManager: SensorManager) {
+    private var mLastAccelerometerValue: SensorEvent? = null
+    private var mLastGravityValue: SensorEvent? = null
+
+    private var mDetectFlag = false
+    private val mDetectInterval: Long = 60  // milliseconds
+    private val mMotionThreshold: Float = 6.0F  // m/s^2
+    private val mDotProductThreshold: Float = 0.8F // Size of dot product
+    private val mDetectHandler = Handler()
+
+    private enum class VoteMotionEvent {
+        STABLE, ACC_UP, ACC_DOWN
+    }
+    private var mMotionStableCount = 0
+    private val mMotionStableThreshold = 4
+    private var mMotionState = VoteMotionEvent.STABLE
+
+    constructor(sensorManager: SensorManager,
+                handUpCallback: ()->Unit,
+                handDownCallback: ()->Unit) {
         mSensorManager = sensorManager
         mListenerLinearAccelerometer = LinearAccelerometerListener(this::onAccelerometerUpdate)
         mListenerGravity = GravityListener(this::onGravityUpdate)
+        mHandUpCallback = handUpCallback
+        mHandDownCallback = handDownCallback
         mSensorLinearAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
         mSensorGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
     }
 
     fun startDetection() {
         mSensorManager.registerListener(mListenerLinearAccelerometer, mSensorLinearAccelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL)
+                SensorManager.SENSOR_DELAY_UI)
         mSensorManager.registerListener(mListenerGravity, mSensorGravity,
-                SensorManager.SENSOR_DELAY_NORMAL)
+                SensorManager.SENSOR_DELAY_UI)
+        mMotionStableCount = 0
+        mMotionState = VoteMotionEvent.STABLE
+        mDetectFlag = true;
+        detectVoteMotion()
     }
 
     fun stopDetection() {
         mSensorManager.unregisterListener(mListenerLinearAccelerometer)
         mSensorManager.unregisterListener(mListenerGravity)
+        mDetectHandler.removeCallbacks(this::detectVoteMotion)
+        mDetectFlag = false
     }
 
     fun setGravityUpdateCallback(callback: TypeSensorCallback) {
@@ -80,11 +125,79 @@ class VoteMotionDetector {
         mAccelerometerUpdateCallback = callback
     }
 
+    private fun updateMotionState(nextMotionState: VoteMotionEvent) {
+        when (mMotionState) {
+            VoteMotionEvent.ACC_DOWN -> {
+                when (nextMotionState) {
+                    VoteMotionEvent.ACC_UP -> mHandDownCallback?.invoke()
+                    else -> {}
+                }
+            }
+            VoteMotionEvent.ACC_UP -> {
+                when (nextMotionState) {
+                    VoteMotionEvent.ACC_DOWN -> mHandUpCallback?.invoke()
+                    else -> {}
+                }
+            }
+            else -> {}
+        }
+        if (mMotionState != nextMotionState) {
+            Log.d(TAG, "updateVoteMotionState $mMotionState -> $nextMotionState")
+            mMotionState = nextMotionState
+        }
+    }
+
+    fun detectVoteMotion() {
+        if (mLastAccelerometerValue == null || mLastAccelerometerValue == null) {
+            mDetectHandler.postDelayed(this::detectVoteMotion, mDetectInterval)
+            return
+        }
+
+        val aVec = Vector3(
+                mLastAccelerometerValue!!.values[0],
+                mLastAccelerometerValue!!.values[1],
+                mLastAccelerometerValue!!.values[2])
+        val gVec = Vector3(
+                mLastGravityValue!!.values[0],
+                mLastGravityValue!!.values[1],
+                mLastGravityValue!!.values[2])
+
+        if (aVec.length < mMotionThreshold) {
+            if (++mMotionStableCount > mMotionStableThreshold) updateMotionState(VoteMotionEvent.STABLE)
+            if (mDetectFlag) mDetectHandler.postDelayed(this::detectVoteMotion, mDetectInterval)
+            return
+        }
+
+        val aUnitVec = aVec.getUnitVector()
+        val gUnitVec = gVec.getUnitVector()
+
+        val dotVal = gUnitVec * aUnitVec
+
+        if (Math.abs(dotVal) < mDotProductThreshold) {
+            if (++mMotionStableCount > mMotionStableThreshold) updateMotionState(VoteMotionEvent.STABLE)
+            if (mDetectFlag) mDetectHandler.postDelayed(this::detectVoteMotion, mDetectInterval)
+            return
+        }
+        mMotionStableCount = 0
+
+        if (dotVal < 0) {
+            Log.d(TAG, "[Acceleration DOWN] AccLength: ${aVec.length} DotVal: $dotVal")
+            updateMotionState(VoteMotionEvent.ACC_DOWN)
+        } else {
+            Log.d(TAG, "[Acceleration UP] AccLength: ${aVec.length} DotVal: $dotVal")
+            updateMotionState(VoteMotionEvent.ACC_UP)
+        }
+
+        if (mDetectFlag) mDetectHandler.postDelayed(this::detectVoteMotion, mDetectInterval)
+    }
+
     fun onAccelerometerUpdate(event: SensorEvent) {
+        mLastAccelerometerValue = event
         mAccelerometerUpdateCallback?.invoke(event.values[0], event.values[1], event.values[2])
     }
 
     fun onGravityUpdate(event: SensorEvent) {
+        mLastGravityValue = event
         mGravityUpdateCallback?.invoke(event.values[0], event.values[1], event.values[2])
     }
 }
