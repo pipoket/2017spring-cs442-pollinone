b3b1d86f932b6078bfb71a63cd7a5b7f38f4b493
Rohjoon Myung <rohjoon@devsisters.com>
2017-05-14 20:40:24 +0900

    나머지 화면들 add

---
+++ proj/pollinone/app/src/main/java/com/cs442/sexysuckzoo/pollinone/WaitingVoteToStart.java
@@ -0,0 +1,13 @@
+package com.cs442.sexysuckzoo.pollinone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaitingVoteToStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_vote_to_start);
+    }
+}
