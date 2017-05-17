c5cdd1dde00ef600cd0913ed88dcbb44622b2927
Woosuk Suh <pipoket88@gmail.com>
2017-05-17 19:48:01 +0900

    FFT and basic DTMF detection PoC is done (but not usable due to too much false positives)

--- pollinone_transoversound_dev/app/src/main/java/info/pipoket/pollinone_transoversound_dev/AudioDTMFReceiver.kt
+++ pollinone_transoversound_dev/app/src/main/java/info/pipoket/pollinone_transoversound_dev/AudioDTMFReceiver.kt
@@ -1,40 +1,70 @@
 package info.pipoket.pollinone_transoversound_dev
 
 import android.media.AudioFormat
-import android.media.AudioManager
 import android.media.AudioRecord
-import android.media.AudioTrack
 import android.media.MediaRecorder
 import android.util.Log
-import kotlin.experimental.and
+import org.jtransforms.fft.DoubleFFT_1D
+
+
 
 /**
  * Created by pipoket on 2017. 5. 16..
  */
 
-class AudioDTMFReceiver : Thread() {
-    private var stopped = false
+class AudioDTMFReceiver() : Thread() {
+    private var mStopped = true
 
     enum class ReceiverState {
+        INIT,
         SEARCH_PREAMBLE,
         RECEIVE_DATA,
         RECEIVE_DONE,
     }
 
+    val TONEMAP = listOf(
+            listOf(2692, 5433),   // 0
+            listOf(2692, 4190),   // 1
+            listOf(2844, 4190),   // 2
+            listOf(3105, 4190),   // 3
+            listOf(2692, 4549),   // 4
+            listOf(2844, 4549),   // 5
+            listOf(3105, 4549),   // 6
+            listOf(2692, 4925),   // 7
+            listOf(2844, 4925),   // 8
+            listOf(3105, 4925),   // 9
+            listOf(2692, 5433)    // preamble
+    )
+
+    private var permGranted = false
+
     private val mSamplingRate = 44100
-    private val mBlockSize = 1024
-    private var mState = ReceiverState.SEARCH_PREAMBLE
+    private var mState = ReceiverState.INIT
+    private val mThreshold = 1.0
 
     /**
      * Give the thread high priority so that it's not canceled unexpectedly, and start it
      */
     init {
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO)
-        start()
+    }
+
+    override fun start() {
+        if (mStopped) {
+            mStopped = false
+            super.start()
+        }
     }
 
     override fun run() {
-        Log.i("AudioDTMFReceiver [$state]", "Running Audio Thread")
+        Log.i("AudioDTMFReceiver [$mState]", "Running Audio Thread")
+
+        while (mState < ReceiverState.SEARCH_PREAMBLE) {
+            if (mStopped)
+                return
+            Log.v("AudioDTMFReceiver [$mState]", "Waiting for permission")
+            sleep(500)
+        }
 
         /*
          * Initialize buffer to hold continuously recorded audio data, start recording, and start
@@ -45,72 +75,110 @@ class AudioDTMFReceiver : Thread() {
                 AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT)
 
+        var bufferSizeCandidate = 2
+        while (bufferSizeCandidate < minBufferSize)
+            bufferSizeCandidate *= 2
+        val bufferSize = bufferSizeCandidate
+
+        Log.v("AudioDTMFReceiver [$mState]",
+                "minBufferSize = $minBufferSize, bufferSize = $bufferSize")
+
         val recorder = AudioRecord(
-                MediaRecorder.AudioSource.MIC,
+                MediaRecorder.AudioSource.VOICE_RECOGNITION,
                 mSamplingRate,
                 AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT,
                 minBufferSize)
 
         try {
-            val dataBuffer = ByteArray(mBlockSize)
-            val fftBuffer = ArrayList<Complex>(mBlockSize)
+            val dataBuffer = ShortArray(bufferSize)
+            val fftBuffer = DoubleArray(bufferSize * 2)
+            val fft = DoubleFFT_1D(bufferSize.toLong())
             recorder.startRecording()
 
-            while (!stopped && mState < ReceiverState.RECEIVE_DONE) {
-                Log.i("Map", "Writing new data to buffer");
-                recorder.read(dataBuffer, 0, mBlockSize)
+            while (!mStopped && mState < ReceiverState.RECEIVE_DONE) {
+                val readSize = recorder.read(dataBuffer, 0, bufferSize)
 
+                for (i in 0..readSize-1) {
+                    fftBuffer[(2 * i)] = dataBuffer[i] / 32768.0
+                    fftBuffer[(2 * i) + 1] = 0.0
+                }
+                fft.complexForward(fftBuffer)
+                val peakList = searchPeaks(fftBuffer)
 
+/*
                 val bytesPerSample = 2            // As it is 16bit PCM
                 val amplification: Double = 100.0 // choose a number as you like
                 var index = 0
                 var floatIndex = 0
-                while(index < mBlockSize - bytesPerSample + 1) {
-                    var sample: Double = 0.0;
-                    for (b in 0..bytesPerSample) {
-                        var v = dataBuffer[index + b];
+                while (index < readSize - bytesPerSample) {
+                    var sample: Double = 0.0
+                    for (b in 0..bytesPerSample - 1) {
+                        var v = dataBuffer[index + b]
                         if (b < bytesPerSample - 1 || bytesPerSample == 1)
                             v = v and 0xFF.toByte()
                         sample += v.toInt().shl(b * 8)
                     }
-                    val sample32 = amplification * (sample / 32768.0);
-                    dataBuffer[floatIndex] = sample32.toByte();
+                    val sample32 = amplification * (sample / 32768.0)
+                    dataBuffer[floatIndex] = sample32.toByte()
 
                     index += bytesPerSample
                     floatIndex += 1
                 }
-
-                for (i in 0..mBlockSize)
-                    fftBuffer[i] = Complex(dataBuffer[i].toDouble(), 0.0)
-                val fftResult = FFT.fft(fftBuffer)
-
-                for(i in 0..mBlockSize/2) {
-                    val frequency = i * mSamplingRate / mBlockSize;
-                    Log.d("FFT", " $frequency: ${fftResult[i]}")
-                }
+*/
 
 
                 when (mState) {
-                    ReceiverState.SEARCH_PREAMBLE -> searchPreamble()
-                    ReceiverState.RECEIVE_DATA -> receiveData()
+                    ReceiverState.SEARCH_PREAMBLE -> searchPreamble(peakList)
+                    ReceiverState.RECEIVE_DATA -> receiveData(peakList)
                 }
             }
         } catch (x: Throwable) {
-            Log.w("AudioDTMFReceiver [$state]", "Error reading voice audio $x")
+            Log.e("AudioDTMFReceiver [$mState]", "Error reading voice audio $x", x)
         } finally {
             recorder.stop()
             recorder.release()
+            close()
         }/*
          * Frees the thread's resources after the loop completes so that it can be run again
          */
     }
 
-    fun searchPreamble() {
+    fun searchPeaks(fftBuffer: DoubleArray): ArrayList<Double> {
+        val peaks = ArrayList<Double>()
 
+        for(i in 0..fftBuffer.size - 1 step 2) {
+            val frequency = (i.toDouble() * mSamplingRate) / fftBuffer.size
+            if (frequency > mSamplingRate / 2.0)
+                break
+
+            if ((frequency < 2700 && frequency > 2690) ||
+                    (frequency < 2850 && frequency > 2840) ||
+                    (frequency < 3110 && frequency > 3100) ||
+                    (frequency < 4195 && frequency > 4185) ||
+                    (frequency < 4555 && frequency > 4540) ||
+                    (frequency < 4930 && frequency > 4920) ||
+                    (frequency < 5440 && frequency > 5430)) {
+                val real = fftBuffer[i]
+                val imag = fftBuffer[i + 1]
+                val lenPow = (real * real) + (imag * imag)
+                if (mThreshold < lenPow) {
+                    peaks.add(frequency)
+                }
+            }
+        }
+
+        return peaks
     }
 
-    fun receiveData() {
+    fun searchPreamble(peakList: ArrayList<Double>) {
+        if (peakList.isEmpty())
+            Log.i("AudioDTMFReceiver [$mState]", "EMPTY!")
+        for(peak in peakList)
+            Log.i("AudioDTMFReceiver [$mState]", "Peak at $peak")
+    }
+
+    fun receiveData(peakList: ArrayList<Double>) {
 
     }
 
@@ -118,7 +186,13 @@ class AudioDTMFReceiver : Thread() {
      * Called from outside of the thread in order to stop the recording/playback loop
      */
     fun close() {
-        stopped = true
+        mStopped = true
+        mState = ReceiverState.INIT
+    }
+
+    fun notifyPermissionGrant() {
+        if (mState < ReceiverState.SEARCH_PREAMBLE)
+            mState = ReceiverState.SEARCH_PREAMBLE
     }
 
 }
--- pollinone_transoversound_dev/app/src/main/java/info/pipoket/pollinone_transoversound_dev/MainActivity.kt
+++ pollinone_transoversound_dev/app/src/main/java/info/pipoket/pollinone_transoversound_dev/MainActivity.kt
@@ -1,23 +1,57 @@
 package info.pipoket.pollinone_transoversound_dev
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     val dtmfReceiver = AudioDTMFReceiver()
 
+    companion object {
+        val PERM_REQUEST_CODE_RECORD = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermission()
         dtmfReceiver.start()
     }
 
     override fun onResume() {
+        super.onResume()
         dtmfReceiver.start()
     }
 
     override fun onPause() {
+        super.onPause()
         dtmfReceiver.close()
+    }
+
+    fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            dtmfReceiver.notifyPermissionGrant()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.RECORD_AUDIO), PERM_REQUEST_CODE_RECORD)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode != PERM_REQUEST_CODE_RECORD)
+            return
+
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            return
+
+        dtmfReceiver.notifyPermissionGrant()
     }
 }
