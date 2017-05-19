6e2b67d604322e5d7f206f198aae7ffd6968dc47
Rohjoon Myung <rohjoon@devsisters.com>
2017-05-19 22:47:24 +0900

    결과 화면 표시 로직까지 완료

--- proj/pollinone/app/src/main/java/com/cs442/sexysuckzoo/pollinone/VotingResult.java
+++ proj/pollinone/app/src/main/java/com/cs442/sexysuckzoo/pollinone/VotingResult.java
@@ -1,13 +1,35 @@
 package com.cs442.sexysuckzoo.pollinone;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class VotingResult extends AppCompatActivity {
+    LinearLayout mLinearLayout = null;
+
+    String[] result = {"Choice#1 : 20", "Choice#2 : 3110" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voting_result);
+
+        mLinearLayout = (LinearLayout) findViewById(R.id.linearLayoutResult);
+        mLinearLayout.removeAllViews();
+
+        for (String s : result){
+            mLinearLayout.addView(getTextView(s));
+        }
+
+    }
+
+    private TextView getTextView(String txt) {
+        TextView tv = new TextView(getApplicationContext());
+        tv.setText(txt);
+        tv.setTextSize(30);
+        tv.setTextColor(Color.BLACK);
+        return tv;
     }
 }
