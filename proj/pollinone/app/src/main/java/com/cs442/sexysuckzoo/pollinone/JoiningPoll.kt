2818fa7acf47816457c7c2426a6b39a6dfd13b26
Rohjoon Myung <rohjoon@devsisters.com>
2017-05-20 01:21:43 +0900

    poll join 할 때 subscribe만 하여 roomId를 보여주도록

--- proj/pollinone/app/src/main/java/com/cs442/sexysuckzoo/pollinone/JoiningPoll.kt
+++ proj/pollinone/app/src/main/java/com/cs442/sexysuckzoo/pollinone/JoiningPoll.kt
@@ -3,32 +3,121 @@ package com.cs442.sexysuckzoo.pollinone
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ListView
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.messages.Message
+import com.google.android.gms.nearby.messages.MessageListener
+import com.google.android.gms.nearby.messages.SubscribeOptions
+import org.json.JSONObject
 
-class JoiningPoll : AppCompatActivity() {
+class JoiningPoll : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks  {
+    private var mLinearLayout : LinearLayout? = null
+    private var mPollList = ArrayList<String>()
+
+    private var mGoogleApiClient: GoogleApiClient? = null
+    private var mMessageListener: MessageListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_poll)
 
-        val ll = findViewById(R.id.roomList) as LinearLayout
-        run {
-            // set joining poll information
-            val b = Button(applicationContext)
-            b.text = "test poll"
-            b.tag = "1"
-            b.setOnClickListener { v -> joiningPoll(v) }
-            ll.addView(b)
+        mLinearLayout = findViewById(R.id.roomList) as LinearLayout
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .build()
+
+        // subscribe model
+        mMessageListener = object : MessageListener()
+        {
+            override fun onFound(message: Message) {
+                var messageAsString = String(message.getContent());
+                var json : JSONObject? = JSONObject(messageAsString)
+
+                if (json?.has("roomId") as Boolean) {
+                    mPollList.add(json?.getString("roomId"))
+                    refreshUI()
+                }
+                Log.d("JoiningPoll", "Found message: " + messageAsString);
+            }
+
+            override fun onLost(message: Message) {
+                var messageAsString = String(message.getContent());
+                var json : JSONObject? = JSONObject(messageAsString)
+                if (json?.has("roomId") as Boolean) {
+                    mPollList.remove(json?.getString("roomId"))
+                    refreshUI()
+                }
+
+                Log.d("JoiningPoll", "Lost sight of message: " + messageAsString);
+            }
         }
+        refreshUI()
     }
 
+    override fun onStart() {
+        super.onStart()
+        mGoogleApiClient?.connect()
+    }
+
+    override fun onStop() {
+        unsubscribe()
+        if (mGoogleApiClient?.isConnected() as Boolean) {
+            mGoogleApiClient?.disconnect()
+        }
+        super.onStop()
+    }
+
+
     fun joiningPoll(v: View) {
+        var idx = Integer.parseInt(v.tag as String)
+        var roomId = mPollList[idx]
+        // TODO : send message to joining the room.
+        // TODO : Send an joining request to server
         if (v.tag === "1") {
             val intent = Intent(applicationContext, WaitingVoteToStart::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun refreshUI() {
+        mLinearLayout?.removeAllViews()
+        var idx = 0;
+        for (str in mPollList)
+        {
+            // set joining poll information
+            val b = Button(applicationContext)
+            b.text = str
+            b.tag = idx
+            b.setOnClickListener { v -> joiningPoll(v) }
+            mLinearLayout?.addView(b)
+        }
+    }
+
+    override fun onConnectionFailed(result: ConnectionResult) {
+        Log.e("JoiningPoll", "onConnectionFailed")
+    }
+    override fun onConnected(var1: Bundle?) {
+        subscribe()
+    }
+    override fun onConnectionSuspended(var1: Int) {}
+
+    // Subscribe to receive messages.
+    private fun subscribe() {
+        Log.i("JoiningPoll", "Subscribing.")
+        Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener, SubscribeOptions.DEFAULT)
+    }
+
+    private fun unsubscribe() {
+        Log.i("JoiningPoll", "Unsubscribing.")
+        Nearby.Messages.unsubscribe(mGoogleApiClient, mMessageListener)
     }
 }
