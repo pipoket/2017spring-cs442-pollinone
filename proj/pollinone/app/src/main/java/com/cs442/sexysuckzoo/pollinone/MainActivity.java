09c978e85908585f530e32569b2d111b335b798c
Rohjoon Myung <rohjoon@devsisters.com>
2017-05-19 21:16:04 +0900

    vote 하는 유저에 대한 화면 구성 연결

--- proj/pollinone/app/src/main/java/com/cs442/sexysuckzoo/pollinone/MainActivity.java
+++ proj/pollinone/app/src/main/java/com/cs442/sexysuckzoo/pollinone/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cs442.sexysuckzoo.pollinone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // button callback
+    public void joiningPoll(View v) {
+        Intent intent = new Intent(this, SearchingPoll.class);
+        startActivity(intent);
+    }
+
+    // button callback
+    public void creatingPoll(View v) {
+        Intent intent = new Intent(this, CreatingPoll.class);
+        startActivity(intent);
     }
 }
